cd72ef158e14c79366e4cfa0256a26dc9264f635
gonzamar13 <marcosgonzs13@gmail.com>
2025-11-29 09:16:32 -0300

    Se cambia la api

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 // Configuración Global de la API
-const BASE_URL = "https://rei-dos-metais-backend-gd9kgn-f78151-109-199-112-8.traefik.me";
+const BASE_URL = "https://back.mindcube.cloud";
 
-// Definimos las rutas exactas según tu Swagger (OpenAPI)
-// IMPORTANTE: Tu backend pide barra al final (/clientes/), así que la incluimos aquí.
+
 window.API_URL = {
     clientes: `${BASE_URL}/clientes/`,
     productos: `${BASE_URL}/productos/`,
